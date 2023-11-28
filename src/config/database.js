72c2c44eaa88ddd3397c20b941ae2be0469c18ad
Giovanni Urdaneta <giovanniurdanetatulli@gmail.com>
2023-11-27 21:08:26 -0400

    pool connections increased to 25

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,12 @@
-require('dotenv/config.js') // cargando variables de entorno
+require("dotenv/config.js"); // cargando variables de entorno
 
-const { Pool } = require('pg')
+const { Pool } = require("pg");
 
-const uri = process.env.DB_URI
+const uri = process.env.DB_URI;
 
-const pool = new Pool({ connectionString: uri })
+const pool = new Pool({ 
+    connectionString: uri,
+    max: 25 
+});
 
-module.exports = pool
+module.exports = pool;
